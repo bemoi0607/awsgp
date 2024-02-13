c648835221bd75dfdbe8a330da7deb1aa7b65e82
facialjun <facialjun@gmail.com>
2024-02-13 17:10:47 +0900

    reset password

--- screens/ForgotPasswordScreen.tsx
+++ screens/ForgotPasswordScreen.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { View,Text, KeyboardAvoidingView,Image,Dimensions, TextInput, TouchableOpacity } from 'react-native'
 import { MainScreens, MainStackParamList } from '../stacks/Navigator';
 import { resetPassword, type ResetPasswordOutput } from 'aws-amplify/auth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
 const screenWidth = Dimensions.get('screen').width;
@@ -21,18 +22,25 @@ interface ForgotPassWordScreenProps {
 const ForgotPasswordScreen: React.FunctionComponent<ForgotPassWordScreenProps> = ({navigation}) => {
     
     const [username, setUsername] = useState<string>('');
-    // const [forgotPassword, setForgotPassword] = useState<string>('');
 
-    // const isForgotPasswordEmpty = () => {
-    //     return forgotPassword.trim().length === 0;
-    // };
+    const storeUsername = async (username: string) => {
+        try {
+            await AsyncStorage.setItem('username', username);
+        } catch (error) {
+            console.log('AsyncStorage Error: ', error);
+        }
+    };
 
+    const isUsernameEmpty = () => {
+        return username.trim().length === 0;
+    };
     
 
     const handleResetPassword= async() => {
     try {
         const output = await resetPassword({ username });
         handleResetPasswordNextSteps(output);
+        await storeUsername(username);
         navigation.navigate(MainScreens.NewPassword)
     } catch (error) {
         console.log(error);
@@ -75,8 +83,8 @@ const ForgotPasswordScreen: React.FunctionComponent<ForgotPassWordScreenProps> =
                 <TouchableOpacity
                     style={{
                         borderRadius: 15,
-                         backgroundColor:  '#4A7AFF', // 버튼 색상 조건부 설정
-                        // backgroundColor: isForgotPasswordEmpty() ? 'gray' : '#4A7AFF', // 버튼 색상 조건부 설정
+                        //  backgroundColor:  '#4A7AFF', // 버튼 색상 조건부 설정
+                        backgroundColor: isUsernameEmpty() ? 'gray' : '#4A7AFF', // 버튼 색상 조건부 설정
                         width: screenWidth * 0.9,
                         justifyContent: 'center',
                         alignItems: 'center',
@@ -84,7 +92,7 @@ const ForgotPasswordScreen: React.FunctionComponent<ForgotPassWordScreenProps> =
                         marginBottom: '3%'
                     }}
                     onPress={handleResetPassword}
-                    // disabled={isForgotPasswordEmpty()} // 버튼 활성화 상태 조건부 설정
+                    disabled={isUsernameEmpty()} // 버튼 활성화 상태 조건부 설정
                 >
                     <Text style={{ fontSize: 18, color: 'white' }}>확인</Text>
                 </TouchableOpacity>
--- screens/NewPasswordScreen.tsx
+++ screens/NewPasswordScreen.tsx
@@ -1,69 +1,89 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, Alert, Dimensions } from 'react-native';
+import { confirmResetPassword, type ConfirmResetPasswordInput } from 'aws-amplify/auth';
 import { StackNavigationProp } from '@react-navigation/stack';
-import React, { useEffect, useState } from 'react'
-import { View,Text, KeyboardAvoidingView,Image,Dimensions, TextInput, TouchableOpacity } from 'react-native'
-import { MainStackParamList } from '../stacks/Navigator';
-import { Auth } from 'aws-amplify/auth';
-
+import { MainScreens, MainStackParamList } from '../stacks/Navigator';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const screenWidth = Dimensions.get('screen').width;
 const screenHeight = Dimensions.get('screen').height;
 
-type ForgotPassWordScreenNavigationProps = StackNavigationProp<
+type NewPassWordScreenNavigationProps = StackNavigationProp<
     MainStackParamList,
-    'ForgotPassword'
+    'NewPassword'
 >;
 
-interface ForgotPassWordScreenProps {
-    navigation: ForgotPassWordScreenNavigationProps; 
+interface NewPassWordScreenProps {
+    navigation: NewPassWordScreenNavigationProps; 
 };
 
 
-const ForgotPasswordScreen: React.FunctionComponent<ForgotPassWordScreenProps> = ({navigation}) => {
 
-    const [forgotPassword, setForgotPassword] = useState<string>('');
+const ConfirmResetPasswordScreen: React.FunctionComponent<NewPassWordScreenProps> = ({ navigation }) => {
+    const [code, setCode] = useState('');
+    const [newPassword, setNewPassword] = useState('');
+    const [username, setUsername] = useState('');
 
-    const isForgotPasswordEmpty = () => {
-        return forgotPassword.trim().length === 0;
+    const isInputValid = () => {
+        return code.trim().length > 0 && newPassword.trim().length > 0;
     };
 
-    // Send confirmation code to user's email or phone number
-async function forgotPasswordSubmit(username: string) {
-    try {
-        const data = await Auth.forgotPassword(username);
-        console.log(data);
-    } catch (err) {
-        console.log(err);
-    }
-}
+    useEffect(() => {
+        // AsyncStorage에서 username 로드
+        const loadUsername = async () => {
+            try {
+                const storedUsername = await AsyncStorage.getItem('username');
+                if (storedUsername !== null) {
+                    setUsername(storedUsername);
+                    console.log(username)
+                }
+            } catch (error) {
+                console.error('Failed to load the username from AsyncStorage', error);
+            }
+        };
+
+        loadUsername();
+    }, []);
 
 
-
-// Collect confirmation code and new password
-async function resetPasswordSubmit(
-    username: string,
-    code: string,
-    newPassword: string
-) {
-    try {
-        const data = await Auth.forgotPasswordSubmit(username, code, newPassword);
-        console.log(data);
-    } catch (err) {
-        console.log(err);
-    }
-}
-
+    const handleSubmit = async () => {
+        try {
+            // 비밀번호 재설정 확인 함수 호출
+            // confirmResetPassword 함수 호출 시 입력 파라미터를 객체 형태로 전달
+            await confirmResetPassword({
+                username, // 사용자명
+                confirmationCode: code, // 사용자로부터 입력받은 확인 코드
+                newPassword // 사용자가 설정한 새 비밀번호
+            });
+            Alert.alert('성공', '비밀번호가 재설정되었습니다.', [
+                { text: 'OK', onPress: () => navigation.navigate(MainScreens.LogIn) } // 성공 시 로그인 화면으로 이동
+            ]);
+        } catch (error) {
+            console.error(error);
+            Alert.alert('실패', '비밀번호 재설정에 실패했습니다.');
+        }
+    };
 
     return (
+
         <View style={{height:'100%',backgroundColor:'white'}}>
         <View style={{justifyContent:'center',alignItems:'center',height:screenHeight*0.5, backgroundColor:'white'}} >
                 <View style={{justifyContent:'center',alignItems:'center'}}>
-                    <Text style={{fontSize:20, fontWeight:'bold', }}>
-                        비밀번호 재설정하기
+                    <Text style={{fontSize:16, fontWeight:'bold', }}>
+                        확인코드와 새로운 비밀번호를 입력해주세요!
                     </Text>
                     <TextInput
                         style={{ borderBottomWidth: 0.5, borderColor: 'gray', fontSize: 23, fontWeight: 'bold',width:screenWidth*0.9, marginTop:'10%' }}
-                        placeholder="이메일을 입력해주세요"
-                        onChangeText={(text) => setForgotPassword(text)}
+                        value={code}
+                        onChangeText={setCode}
+                        placeholder="확인 코드"
+                    />  
+                    <TextInput
+                        style={{ borderBottomWidth: 0.5, borderColor: 'gray', fontSize: 23, fontWeight: 'bold',width:screenWidth*0.9, marginTop:'10%' }}
+                        value={newPassword}
+                        onChangeText={setNewPassword}
+                        placeholder="새 비밀번호"
+                        secureTextEntry={true}
                     />  
                     
                 </View>
@@ -72,15 +92,16 @@ async function resetPasswordSubmit(
                 <TouchableOpacity
                     style={{
                         borderRadius: 15,
-                        backgroundColor: isForgotPasswordEmpty() ? 'gray' : '#4A7AFF', // 버튼 색상 조건부 설정
+                        //  backgroundColor:  '#4A7AFF', // 버튼 색상 조건부 설정
+                        backgroundColor: isInputValid() ? '#4A7AFF' : 'gray', // 버튼 색상 조건부 설정
                         width: screenWidth * 0.9,
                         justifyContent: 'center',
                         alignItems: 'center',
                         height: screenHeight * 0.05,
                         marginBottom: '3%'
                     }}
-                    // onPress={handleContinue}
-                    disabled={isForgotPasswordEmpty()} // 버튼 활성화 상태 조건부 설정
+                    onPress={handleSubmit}
+                    disabled={!isInputValid()} // 버튼 활성화 상태 조건부 설정
                 >
                     <Text style={{ fontSize: 18, color: 'white' }}>확인</Text>
                 </TouchableOpacity>
@@ -88,7 +109,26 @@ async function resetPasswordSubmit(
 
         </View>
         </View>
-    )
-}
+        // <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        //     <Text>확인 코드 입력</Text>
+        //     <TextInput
+        //         value={code}
+        //         onChangeText={setCode}
+        //         placeholder="확인 코드"
+        //         style={{ width: 200, height: 40, borderColor: 'gray', borderWidth: 1 }}
+        //     />
+        //     <TextInput
+        //         value={newPassword}
+        //         onChangeText={setNewPassword}
+        //         placeholder="새 비밀번호"
+        //         secureTextEntry={true}
+        //         style={{ width: 200, height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 10 }}
+        //     />
+        //     <TouchableOpacity onPress={handleSubmit} style={{ marginTop: 20, backgroundColor: 'blue', padding: 10 }}>
+        //         <Text style={{ color: 'white' }}>제출</Text>
+        //     </TouchableOpacity>
+        // </View>
+    );
+};
 
-export default ForgotPasswordScreen
+export default ConfirmResetPasswordScreen;
